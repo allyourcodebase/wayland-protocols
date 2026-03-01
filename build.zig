51df3b24e48fab8a4cae95639063398d99448668
Pablo Alessandro Santos Hugen <phugen@redhat.com>
2026-03-01 15:05:04 -0300

    update hash
    Signed-off-by: Pablo Alessandro Santos Hugen <phugen@redhat.com>

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const upstream = b.dependency("upstream", .{});
+    const upstream = b.dependency("wayland_protocols_c", .{});
 
     b.addNamedLazyPath("root", upstream.path(""));
     inline for (.{ "stable", "staging", "unstable" }) |category|
